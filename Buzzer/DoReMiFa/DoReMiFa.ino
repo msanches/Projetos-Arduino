8da80cbdb0bf32861c2ac4481053f2e3476336d9
Marco Antonio Sanches <msanc.net@gmail.com>
2021-01-07 14:41:48 -0300

    Update DoReMiFa.ino

--- Buzzer/DoReMiFa/DoReMiFa.ino
+++ Buzzer/DoReMiFa/DoReMiFa.ino
@@ -9,10 +9,14 @@
 #define SI  494
 #define DO_2  523
  
-int pinoBuzzer = 6;
+int pinoBuzzer = 7;
  
 int melodia[] = {
   DO, RE, MI, FA, SOL, LA, SI, DO_2
+};
+
+int melodia2[] = {
+  DO, RE, MI, FA, FA, FA, DO, RE, DO, RE, RE, RE, DO, SOL, FA, MI,DO, RE, MI, FA, FA, FA
 };
  
 void setup() {
@@ -21,9 +25,9 @@ void setup() {
 }
  
 void loop() {
-  for(int i=0; i<8; i++)
+  for(int i=0; i<22; i++)
   {
-    tone(pinoBuzzer, melodia[i]);
+    tone(pinoBuzzer, melodia2[i]);
     delay(500);
   }
 }
