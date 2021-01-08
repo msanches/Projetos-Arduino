868d466fe624ebf7ace1fa5ea9ed0fbdfdabfcae
Marco Antonio Sanches <marcosanches.dev@gmail.com>
2021-01-08 15:31:56 -0300

    Update PiscaLed.ino

--- PiscaLed/PiscaLed.ino
+++ PiscaLed/PiscaLed.ino
@@ -1,12 +1,12 @@
 //Projeto arduino pisca led
  
 void setup() {
-  pinMode(10,OUTPUT);
+  pinMode(6,OUTPUT);
 }
  
 void loop() {
-  digitalWrite(10,HIGH);
+  digitalWrite(6,HIGH);
   delay(1000);
-  digitalWrite(10,LOW);
+  digitalWrite(6,LOW);
   delay(1000);
 }
